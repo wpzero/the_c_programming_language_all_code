ff49771f5c5f21c59f20c4c0ee8f9da8d6b9e5c7
wpzero <wpcreep@gmail.com>
2018-05-16 22:48:56 +0800

    Simple-version-grep

---
+++ 5_13/grep.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXLNS 10000
+
+int main(int argc, char *argv[]) {
+  char *lines[MAXLNS];
+  char *buf = NULL;
+  char *ptn;
+  FILE *f;
+  int i, j;
+  i = j = 0;
+  size_t n;
+  n = 0;
+
+  if(argc < 2) {
+    return 1;
+  } else
+    ptn = argv[1];
+
+  if(argc == 2) {
+    f = stdin;
+  } else {
+    f = fopen(argv[2], "r");
+    if(!f) {
+      ferror(f);
+      clearerr(f);
+    }
+  }
+
+  while(getline(&buf, &n, f) != -1) {
+    if(strstr(buf, ptn)) {
+      *(lines+i) = buf;
+      buf = NULL;
+      i++;
+    }
+  }
+
+  if(!feof(f))
+    ferror(f);
+
+  for(j = 0; j < i; j++) {
+    printf("%s", *(lines+j));
+    free(*(lines+j));
+  }
+
+  fclose(f);
+
+  return 0;
+}
